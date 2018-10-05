247fbc747ff588af8e71121246fe65759e33b772
KyuhyeonJang <jgh98090280@gmail.com>
2018-10-05 15:12:11 +0900

    나눗셈

--- calculate.java
+++ calculate.java
@@ -8,7 +8,8 @@ public class Calculator{
     public static int mul(int a, int b){
         return a*b;
     }
-    public static int sum(int a, int b, int c) {
-        return a+b+c;
+    public static int div(int a, int b){
+        return a/b;
     }
+
 }
