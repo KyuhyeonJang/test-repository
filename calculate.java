182c51f6d2d735ea384fd65235cbbe2aba6ab267
KyuhyeonJang <jgh98090280@gmail.com>
2018-10-05 14:34:37 +0900

    sum x 3

--- calculate.java
+++ calculate.java
@@ -5,8 +5,10 @@ public class Calculator{
     public static int minus(int a, int b){
         return a-b;
     }
-    
     public static int mul(int a, int b){
         return a*b;
+    }
+    public static int sum(int a, int b, int c) {
+        return a+b+c;
     }
 }
